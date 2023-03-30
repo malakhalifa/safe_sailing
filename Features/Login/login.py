2fdd03a3afa9d4f1aedb7fa3bcdeb24e9a2fcf92
Andrew Asseily <aa7342@nyu.edu>
2023-03-30 16:18:37 -0400

    Login

---
+++ Features/Login/login.py
@@ -0,0 +1,11 @@
+from kivy.lang import Builder
+from kivymd.app import MDApp
+
+class LoginClass(MDApp):
+    def build(self):
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "BlueGray"
+        return Builder.load_file('login.kv')
+    
+#LoginClass().run()
+
--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 import kivy
-
 kivy.require('2.1.0') # replace with your current kivy version !
 
 from kivy.app import App
 from kivy.uix.label import Label
 
+from kivy.lang import Builder
+from kivymd.app import MDApp
+
+#import features
+
+from Features.Login.login import LoginClass
+
 
 class MyApp(App):
 
-    def build(self):
-        return Label(text='Welcome to Safe Sailing!')  
-
-
+    def build(self):    
+        #return Label(text='Welcome to Safe Sailing!')
+        return LoginClass().build()
+    
 if __name__ == '__main__':
     MyApp().run()
