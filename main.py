e8e607f92b895f4cf6a9bf3ac0a2d3daf0f0ff19
Andrew Asseily <aa7342@nyu.edu>
2023-03-30 11:16:45 -0400

    weird reset

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import kivy
+
 kivy.require('2.1.0') # replace with your current kivy version !
 
 from kivy.app import App
@@ -8,7 +9,7 @@ from kivy.uix.label import Label
 class MyApp(App):
 
     def build(self):
-        return Label(text='Welcome to Safe Sailing!')
+        return Label(text='Welcome to Safe Sailing!')  
 
 
 if __name__ == '__main__':
