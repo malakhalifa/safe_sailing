12f0ec5c3bf8925c390e9376241af4f0e4f13ddc
Malak Khalifa <malakhalifa0@gmail.com>
2023-04-06 02:46:13 -0400

    login

---
+++ Features/Login/login.py
@@ -0,0 +1,18 @@
+from kivy.lang import Builder
+from kivymd.app import MDApp
+from kivy.core.window import Window
+from kivy.core.text import LabelBase
+from kivy.uix.screenmanager import ScreenManager
+
+Window.size = (310, 580)
+
+class LoginClass(MDApp):
+    def build(self):
+        screen_manager = ScreenManager() 
+        screen_manager.add_widget(Builder.load_file('Features/MainPage/main.kv'))
+        screen_manager.add_widget(Builder.load_file('Features/Login/login.kv'))
+        screen_manager.add_widget(Builder.load_file('Features/CreateAccount/CreateAccount.kv'))
+        
+        return screen_manager
+
+
