2393649d0509f8cab0d3d4e4f5618c421336304f
Malak Khalifa <malakhalifa0@gmail.com>
2023-04-12 14:15:23 -0400

    updated

--- Features/Login/login.py
+++ Features/Login/login.py
@@ -4,15 +4,13 @@ from kivy.core.window import Window
 from kivy.core.text import LabelBase
 from kivy.uix.screenmanager import ScreenManager
 
-Window.size = (310, 580)
+# Window.size = (310, 580)
 
-class LoginClass(MDApp):
-    def build(self):
-        screen_manager = ScreenManager() 
-        screen_manager.add_widget(Builder.load_file('Features/MainPage/main.kv'))
-        screen_manager.add_widget(Builder.load_file('Features/Login/login.kv'))
-        screen_manager.add_widget(Builder.load_file('Features/CreateAccount/CreateAccount.kv'))
+# class LoginClass(MDApp):
+#     def build(self):
+#         # screen_manager = ScreenManager() 
+#         # screen_manager.add_widget(Builder.load_file('Features/MainPage/main.kv'))
+#         # screen_manager.add_widget(Builder.load_file('Features/Login/login.kv'))
+#         # screen_manager.add_widget(Builder.load_file('Features/CreateAccount/CreateAccount.kv'))
         
-        return screen_manager
-
-
+#         # return screen_manager
--- main.py
+++ main.py
@@ -2,19 +2,32 @@ import kivy
 kivy.require('2.1.0') # replace with your current kivy version !
 
 from kivy.app import App
-from kivy.uix.label import Label
-
 from kivy.lang import Builder
 from kivymd.app import MDApp
+from kivy.core.text import LabelBase
+from kivy.core.window import Window
+from kivy.uix.screenmanager import ScreenManager
+# from Features.Login.login import LoginClass
+# from Features.CreateAccount.createaccount import CreateAccountClass
+# from Features.RateComment.ratecomment import RateCommentClass
 
-#import features
-from Features.Login.login import LoginClass
+Window.size = (310, 580)
+class MyApp(MDApp):
+    def build(self):
+        screen_manager = ScreenManager() 
+        screen_manager.add_widget(Builder.load_file('Features/MainPage/main.kv'))
+        screen_manager.add_widget(Builder.load_file('Features/Login/login.kv'))
+        screen_manager.add_widget(Builder.load_file('Features/CreateAccount/CreateAccount.kv'))
+        screen_manager.add_widget(Builder.load_file('Features/RateComment/ratecomment.kv'))
 
-class MyApp(App):
+        return screen_manager
 
-    def build(self):    
-        #return Label(text='Welcome to Safe Sailing!')
-        return LoginClass().build()
+# Can't figure out how to integrate multiple .py files into one (main)
+# Can't figure out how to implement functionality in main     
     
 if __name__ == '__main__':
+    LabelBase.register(name = 'MPoppins', fn_regular='Features/FontPoppins/Poppins-Medium.ttf')
+    LabelBase.register(name = 'MPoppinsBold', fn_regular='Features/FontPoppins/Poppins-SemiBold.ttf')
+    LabelBase.register(name = 'BPoppins', fn_regular='Features/FontPoppins/Poppins-SemiBold.ttf')
+
     MyApp().run()
